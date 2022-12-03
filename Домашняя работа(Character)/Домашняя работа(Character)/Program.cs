eec6996a44f6057fd388c954ba15f3dedf3ad12e
MarinaPopovich <117534487+MarinaPopovich@users.noreply.github.com>
2022-12-03 11:21:50 +0300

    Add files via upload

---
+++ Домашняя работа(Character)/Домашняя работа(Character)/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Домашняя_работа_Character_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+    public abstract class Character
+    {
+        public virtual int Health { get; set; }
+        public int Attakc;
+        public int Armor;
+        public Character(int health)
+        {
+            Health = health;
+            Attakc = 70;
+            Armor = 80;
+        }
+        public abstract void Attak();
+    }
+    public class Soldier : Character
+    {
+        public int Attack { get; set; }
+        public Soldier(int health) : base(health)
+        {
+            Health = 100;
+        }
+        public override void Attak()
+        {
+            Health -= 2;
+            Console.WriteLine($" Боец актаковал." +
+                $"  здоровье: {Health}");
+        }
+        public class Archer : Character
+        {
+            public Archer(int health) : base(health)
+            {
+                Health = health;
+            }
+            private int _health;
+            public override int Health
+            {
+                get
+                {
+                    return _health;
+                }
+                set
+                {
+                    _health = value;
+                }
+            }
+            public override void Attak()
+            {
+                Health -= 2;
+                Console.WriteLine($" Лучник актаковал." +
+                    $"  здоровье: {Health}");
+            }
+            public void Dodge()
+            {
+                Armor += 10;
+                Console.WriteLine($" Волк увернулся." +
+                    $" защита: {Armor}");
+            }
+        }
+
+        public class Shield_Bearer : Archer
+        {
+            public Shield_Bearer(int health) : base(health)
+            {
+                Health = health;
+            }
+            public override void Attak()
+            {
+                Health -= 2;
+                Console.WriteLine($" Щитоносец актаковал." +
+                    $"  здоровье: {Health}");
+            }
+            public void Block()
+            {
+                Health -= 5;
+                this.Health += 20;
+                Console.WriteLine($"Щитоносец блокировал удар." +
+                    $"защита: {Armor}, здоровье: {Health}");
+            }
+        }
+    }
+}
+
+
+//}private int _health;
+//        public override int Health;
+//       {get 
+//            {return _health;
+//}
+//    set
+//        {_health = Value;}}
